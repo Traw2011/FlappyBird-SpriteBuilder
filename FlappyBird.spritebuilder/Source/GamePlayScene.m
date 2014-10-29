9d4a16fa2543f370e89e2329206dfaf34fb98174
Traw2011 <tevinharris@Tevins-MacBook-Pro.local>
2014-10-29 14:40:29 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,10 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild:character];
+    
+    
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +19,8 @@
 }
 
 // put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    [character flap];
+}
 
 @end
