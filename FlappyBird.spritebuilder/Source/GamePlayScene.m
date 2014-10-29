e2cfa461477f439428c0a48be5a7034c3c93d2d9
Traw2011 <tevinharris@Tevins-MacBook-Pro.local>
2014-10-29 14:52:05 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -9,6 +9,7 @@
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
+    timeSinceObstacle = 0.0f;
     
     
 }
@@ -16,6 +17,16 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceObstacle += delta;
+    
+    if (timeSinceObstacle) > 2.0f{)
+        {
+        //add new obstacle
+        [self addObstacle];
+        
+        //reset timer
+        timeSinceObstacle = 0.0f;
+        }
 }
 
 // put new methods here
@@ -23,6 +34,7 @@
     [character flap];
     
     [self addObstacle];
+    
 }
 
 @end
